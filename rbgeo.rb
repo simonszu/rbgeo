6237e1207b9c10db9c416eb5d726d6162f45ae54
Simon Szustkowski <mail@simonszu.de>
2014-02-20 14:37:24 +0100

    Mit dem Loginvorgang begonnen

--- rbgeo.rb
+++ rbgeo.rb
@@ -2,6 +2,19 @@
 
 require 'mechanize'
 require 'nokogiri'
+require 'yaml'
+
+CONFIG_FILE = 'config.yaml'
+
+LOGIN_PAGE = 'https://www.geocaching.com/login/'
+
+config = YAML::load(File.open(File.join(File.dirname(__FILE__), CONFIG_FILE)))
+gc_user = config['credentials']['username']
+gc_passwd = config['credentials']['password']
 
 a = Mechanize.new {|agent|
   agent.user_agent_alias = 'Mac Safari'}
+
+a.get(LOGIN_PAGE) do |login_page|
+  start_page = login.page.form_with(:action => )
+end
