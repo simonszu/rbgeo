eaf3a48b5b4c02c7801b7a47071dd548ebf279b9
Simon Szustkowski <mail@simonszu.de>
2014-02-21 11:47:50 +0100

    Schonmal ein paar Attribute der gefundenen Caches extrahiert

--- rbgeo.rb
+++ rbgeo.rb
@@ -10,6 +10,9 @@ CONFIG_FILE = 'config.yaml'
 LOGIN_PAGE = 'https://www.geocaching.com/login/'
 MY_PAGE = 'http://www.geocaching.com/my/logs.aspx'
 
+LOGTYPES = ["Found it", "Didn't find it", "Will attend", "Owner Maintenance", "Needs Maintenance", "Needs Archived", "Archive", "Enable Listing", "Temporarily Disable Listing", "Post Reviewer Note", "Webcam Photo Taken", "Write note", "Attended"]
+CACHETYPES = ["Multi-cache", "Traditional Cache", "Event Cache", "Unknown Cache", "Webcam Cache", "Lost and Found Event Cache", "Earthcache"]
+
 # Load the config
 config = YAML::load(File.open(File.join(File.dirname(__FILE__), CONFIG_FILE)))
 gc_user = config['credentials']['username']
@@ -19,6 +22,9 @@ gc_passwd = config['credentials']['password']
 a = Mechanize.new {|agent|
   agent.user_agent_alias = 'Mac Safari'}
 
+puts "This is rbgeo version 0.1."
+puts "Logging in..."
+
 # Login
 a.get(LOGIN_PAGE) do |login_page|
   login_page.form_with(:name => 'aspnetForm') do |f|
@@ -27,7 +33,24 @@ a.get(LOGIN_PAGE) do |login_page|
   end.click_button
 end
 
+puts "Loading your caches..."
+
 # List my founds
 a.get(MY_PAGE).search("table.Table tr").each do |cachetable|
-  puts cachetable.css("a")[1].inner_text
+  favorite = false
+  name = cachetable.css("a")[1].inner_text.strip
+  logtype = cachetable.css("img")[0]["title"].strip
+  logdate = cachetable.css("td")[2].inner_text.strip
+  if cachetable.css("img").length == 3
+    cachetype = cachetable.css("img")[2]["title"].strip
+    favorite = true
+  else
+    cachetype = cachetable.css("img")[1]["title"].strip
+  end
+  nbsp = Nokogiri::HTML("&nbsp;").text
+  area = cachetable.css("td")[4].inner_text.gsub(nbsp, "").strip
+  log_page = cachetable.css("a")[2]["href"]
+  puts ("#{name} - #{logtype} - #{logdate} - #{cachetype} - #{area} - #{favorite} - #{log_page}")
 end
+
+puts "Bye."
