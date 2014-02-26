15f027a13f016edcaf8815aef6f91c023ef46f6d
Simon Szustkowski <mail@simonszu.de>
2014-02-26 16:58:05 +0100

    Logdates nun im unix timestamp format

--- rbgeo.rb
+++ rbgeo.rb
@@ -16,6 +16,8 @@ MY_PAGE = 'http://www.geocaching.com/my/logs.aspx'
 LOGTYPES = ["Found it", "Didn't find it", "Will attend", "Owner Maintenance", "Needs Maintenance", "Needs Archived", "Archive", "Enable Listing", "Temporarily Disable Listing", "Post Reviewer Note", "Webcam Photo Taken", "Write note", "Attended"]
 CACHETYPES = ["Multi-cache", "Traditional Cache", "Event Cache", "Unknown Cache", "Webcam Cache", "Lost and Found Event Cache", "Earthcache"]
 
+puts "This is rbgeo version 0.1."
+
 # Load the config
 config = YAML::load(File.open(File.join(File.dirname(__FILE__), CONFIG_FILE)))
 gc_user = config['credentials']['username']
@@ -31,7 +33,7 @@ begin
     db = SQLite3::Database.open CACHEDB
   else
     db = SQLite3::Database.new CACHEDB
-    db.execute "CREATE TABLE IF NOT EXISTS Caches(Id INTEGER PRIMARY KEY, Name TEXT, Logtype TEXT, Logdate INT, Cachetype TEXT, Area TEXT, Favorite INTEGER, Log TEXT)"
+    db.execute "CREATE TABLE IF NOT EXISTS caches(id INTEGER PRIMARY KEY, name TEXT, logtype TEXT, logdate INTEGER, cachetype TEXT, area TEXT, favorite INTEGER, log TEXT)"
   end
 rescue SQLite3::Exception => e
   puts "Fehler beim Zugriff oder Anlegen der Datenbank."
@@ -40,7 +42,6 @@ rescue SQLite3::Exception => e
   exit
 end
 
-puts "This is rbgeo version 0.1."
 puts "Logging in..."
 
 # Login
@@ -55,20 +56,22 @@ puts "Loading your caches..."
 
 # List my founds
 a.get(MY_PAGE).search("table.Table tr").each do |cachetable|
-  favorite = false
+  favorite = 0
   name = cachetable.css("a")[1].inner_text.strip
   logtype = cachetable.css("img")[0]["title"].strip
-  logdate = cachetable.css("td")[2].inner_text.strip
+  cachetable.css("td")[2].inner_text.strip.match(/(\d{2})\/(\d{2})\/(\d{4})/)
+  logdate = Time.new($3, $1, $2).to_i
   if cachetable.css("img").length == 3
     cachetype = cachetable.css("img")[2]["title"].strip
-    favorite = true
+    favorite = 1
   else
     cachetype = cachetable.css("img")[1]["title"].strip
   end
   nbsp = Nokogiri::HTML("&nbsp;").text
   area = cachetable.css("td")[4].inner_text.gsub(nbsp, "").strip
   log_page = cachetable.css("a")[2]["href"]
-  puts ("#{name} - #{logtype} - #{logdate} - #{cachetype} - #{area} - #{favorite} - #{log_page}")
+  db.execute("INSERT INTO caches (name, logtype, logdate, cachetype, area, favorite, log) VALUES (?, ?, ?, ?, ?, ?, ?)", [name, logtype, logdate, cachetype, area, favorite, log_page])
 end
 
+db.close
 puts "Bye."
