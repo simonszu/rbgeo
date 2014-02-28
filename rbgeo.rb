b350153fb8f27101ee774f67cdb52a10a1784fce
Simon Szustkowski <mail@simonszu.de>
2014-02-28 16:23:50 +0100

    Datenbank wird nun geupdatet, wenn sich am Cache grundlegende Details wie Name, Status oder Containergroesse aendern. Will Attend Logs werden ignoriert

--- rbgeo.rb
+++ rbgeo.rb
@@ -47,7 +47,7 @@ a.get(LOGIN_PAGE) do |login_page|
   end.click_button
 end
 
-puts "Loading your caches..."
+puts "Loading caches which are updated or not in the DB at all..."
 
 # List my founds
 a.get(MY_PAGE).search("table.Table tr").each do |cachetable|
@@ -57,6 +57,9 @@ a.get(MY_PAGE).search("table.Table tr").each do |cachetable|
   status = "Available"
   name = cachetable.css("a")[1].inner_text.strip
   logtype = cachetable.css("img")[0]["title"].strip
+  if (logtype.eql? "Will Attend")
+    next
+  end
   if cachetable.css("a > span[class = 'Strike']").length > 0
     status = "Disabled"
   elsif cachetable.css("a > span[class = 'Strike OldWarning']").length > 0
@@ -75,48 +78,51 @@ a.get(MY_PAGE).search("table.Table tr").each do |cachetable|
   cachetable.css("a")[0]["href"].match(/http:\/\/www\.geocaching\.com\/seek\/cache_details\.aspx\?guid=(.+)/)
   guid = $1
 
-  # Get detailed details
-  print "Loading details for #{name}..."
-  begin
-    detailpage = a.get(cachetable.css("a")[1]["href"])
-    owner = detailpage.search("div[id = 'ctl00_ContentBody_mcd1'] a")[0].inner_text
-    detailpage.search("span[id = 'ctl00_ContentBody_uxLegendScale'] img")[0]["alt"].match(/(.{1,3}) out of 5/)
-    difficulty = $1.to_f
-    detailpage.search("span[id = 'ctl00_ContentBody_Localize12'] img")[0]["alt"].match(/(.{1,3}) out of 5/)
-    terrain = $1.to_f
-    detailpage.search("span.minorCacheDetails img")[0]["alt"].match(/Size: (.+)/)
-    size = $1
-    coords = detailpage.search("span[id = 'uxLatLon']")[0].inner_text
-    detailpage.search("div[id='ctl00_ContentBody_mcd2']")[0].inner_text.match(/(\d{2})\/(\d{2})\/(\d{4})/)
-    hiddendate = Time.new($3, $1, $2).to_i
-    if detailpage.search("span.favorite-value").length > 0
-      favcount = detailpage.search("span.favorite-value")[0].inner_text
-    else
+  # Check if detailed details differ from stored details or check if the cache isn't stored at all
+  storedcache = db.execute("SELECT * FROM caches WHERE guid = '#{guid}'")
+  if (storedcache.empty?) || (storedcache[0][2] != name) || (storedcache[0][14] != logtype) || (storedcache[0][11] != status) || (storedcache[0][15].to_i != logdate) || (storedcache[0][16].to_i != favorite) || (storedcache[0][9] != area)
+    # Get detailed details
+    print "Loading details for #{name}..."
+    begin
+      detailpage = a.get(cachetable.css("a")[1]["href"])
+      owner = detailpage.search("div[id = 'ctl00_ContentBody_mcd1'] a")[0].inner_text
+      detailpage.search("span[id = 'ctl00_ContentBody_uxLegendScale'] img")[0]["alt"].match(/(.{1,3}) out of 5/)
+      difficulty = $1.to_f
+      detailpage.search("span[id = 'ctl00_ContentBody_Localize12'] img")[0]["alt"].match(/(.{1,3}) out of 5/)
+      terrain = $1.to_f
+      detailpage.search("span.minorCacheDetails img")[0]["alt"].match(/Size: (.+)/)
+      size = $1
+      coords = detailpage.search("span[id = 'uxLatLon']")[0].inner_text
+      detailpage.search("div[id='ctl00_ContentBody_mcd2']")[0].inner_text.match(/(\d{2})\/(\d{2})\/(\d{4})/)
+      hiddendate = Time.new($3, $1, $2).to_i
+      if detailpage.search("span.favorite-value").length > 0
+        favcount = detailpage.search("span.favorite-value")[0].inner_text
+      else
+        favcount = 0
+      end
+      gcid = detailpage.search("span[id='ctl00_ContentBody_CoordInfoLinkControl1_uxCoordInfoCode']")[0].inner_text
+    rescue
+      owner = "N.N"
+      difficulty = 0
+      terrain = 0
+      size = "-"
+      coords = "-"
       favcount = 0
+      hiddendate = 0
+      gcid = "NOT_PUBLISHED"
     end
-    gcid = detailpage.search("span[id='ctl00_ContentBody_CoordInfoLinkControl1_uxCoordInfoCode']")[0].inner_text
-  rescue
-    owner = "N.N"
-    difficulty = 0
-    terrain = 0
-    size = "-"
-    coords = "-"
-    favcount = 0
-    hiddendate = 0
-    gcid = "NOT_PUBLISHED"
-  end
-  print "OK\n"
-  sleep (1..5).to_a.sample
+    print "OK\n"
+    sleep (1..5).to_a.sample
 
-  
-  # Get the log
-  print "Loading log for #{name}..." 
-  log = a.get(cachetable.css("a")[2]["href"]).search("span[id = 'ctl00_ContentBody_LogBookPanel1_LogText']")[0].inner_text.strip
-  print "OK\n"
-  sleep (1..5).to_a.sample
+    # Get the log
+    print "Loading log for #{name}..." 
+    log = a.get(cachetable.css("a")[2]["href"]).search("span[id = 'ctl00_ContentBody_LogBookPanel1_LogText']")[0].inner_text.strip
+    print "OK\n"
+    sleep (1..5).to_a.sample
 
-  # Insert into DB
-  db.execute("INSERT INTO caches (gcid, name, status, owner, difficulty, terrain, size, hiddendate, coords, favcount, logtype, logdate, cachetype, area, favorite, log, guid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [gcid, name, status, owner, difficulty, terrain, size, hiddendate, coords, favcount, logtype, logdate, cachetype, area, favorite, log, guid]) 
+    # Insert into DB
+    db.execute("INSERT INTO caches (gcid, name, status, owner, difficulty, terrain, size, hiddendate, coords, favcount, logtype, logdate, cachetype, area, favorite, log, guid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [gcid, name, status, owner, difficulty, terrain, size, hiddendate, coords, favcount, logtype, logdate, cachetype, area, favorite, log, guid])
+  end 
 end
 
 db.close
