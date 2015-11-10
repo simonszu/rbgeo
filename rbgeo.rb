b02591c52ac753a011dc8bd32dc8a541264d698b
Simon Szustkowski <mail@simonszu.de>
2015-11-10 13:26:29 +0100

    Bessere hilfe

--- rbgeo.rb
+++ rbgeo.rb
@@ -15,41 +15,46 @@ opts = Slop.parse do |o|
   o.bool '-o', '--parse-owns', 'parse the owns'
   o.separator "Generating website functions: "
   o.bool '-w', '--generate-website', 'generate the website'
-  o.on '--help' do
+  o.on  '-h', '--help', 'outputs the help' do
     puts o
     exit
   end
 end
 
-# Save the starting time for benchmarking
-beginning_time = Time.now
+# Check if there are command line options:
+if ((opts.parse_logs?) || (opts.parse_owns?) || (opts.generate_website?))
+  # Save the starting time for benchmarking
+  beginning_time = Time.now
 
-project_root = File.dirname(File.absolute_path(__FILE__))
-Dir.glob(project_root + '/lib/*.rb', &method(:require))
-puts "This is rbgeo version 0.1."
+  project_root = File.dirname(File.absolute_path(__FILE__))
+  Dir.glob(project_root + '/lib/*.rb', &method(:require))
+  puts "This is rbgeo version 0.1."
 
-# Load the config and make it globally accessible for every function
-$config = YAML::load(File.open(File.join(File.dirname(__FILE__), CONFIG_FILE)))
+  # Load the config and make it globally accessible for every function
+  $config = YAML::load(File.open(File.join(File.dirname(__FILE__), CONFIG_FILE)))
 
 
-# Connect and initialize db
-init_db
+  # Connect and initialize db
+  init_db
 
-if opts.parse_logs?
-  parse_logs_gc()
+  if opts.parse_logs?
+    parse_logs_gc()
+  end
+  if opts.parse_owns?
+    parse_owns_gc()
+  end
+  if opts.generate_website?
+    generate_website()
+  end
+
+  ActiveRecord::Base.connection.close
+
+  # Check the end time and calculate the running duration
+  end_time = Time.now
+
+  puts "All done. Time elapsed #{(end_time - beginning_time).round(2)} seconds."
+
+  puts "Bye."
+else
+  puts opts
 end
-if opts.parse_owns?
-  parse_owns_gc()
-end
-if opts.generate_website?
-  generate_website()
-end
-
-ActiveRecord::Base.connection.close
-
-# Check the end time and calculate the running duration
-end_time = Time.now
-
-puts "All done. Time elapsed #{(end_time - beginning_time).round(2)} seconds."
-
-puts "Bye."
